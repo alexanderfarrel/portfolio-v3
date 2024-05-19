973ab613225ada611c4b5d7c7a862dd1b4e5f52f
Alexz Desktop <chikofarrel@gmail.com>
2024-05-19 23:02:50 +0700

    update base vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
+  base: "/alexanderfarrel.github.io/",
   server: {
     host: true,
     staticPort: true,
